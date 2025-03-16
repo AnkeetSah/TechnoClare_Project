63e741997dace048cd3f6db7f40ccb828e16260e
AnkeetSah <ankeetkumar7777@gmail.com>
2025-03-16 18:09:07 +0530

    made the car select ui responsive

--- src/pages/SelectCar.jsx
+++ src/pages/SelectCar.jsx
@@ -34,16 +34,16 @@ const SelectCar = () => {
 
   return (
     <div className="w-[90%] mx-auto mt-8">
-      <div className="flex justify-end ">
+      <div className="flex justify-end xs:mb-3 lg:mb-0 ">
       <h4 className="font-AfacadRegular text-sm border border-white text-white px-3 py-1 inline-flex items-center bg-black bg-opacity-40 backdrop-blur-md rounded-md">
           View All Cars <FaArrowRightLong className="ml-2" />
         </h4>
       </div>
 
-      <div class="grid lg:grid-cols-2 gap-6 xs:grid-cols-1   p-4">
+      <div class="lg:grid lg:grid-cols-2 lg:gap-6    lg:p-4  xs:flex xs:flex-col xs:gap-5 xs:flex-wrap">
        {carData.map((car) => (
-         <div class=" h-[260px] flex  ">
-         <div className=" w-1/2 h-[260px]">
+         <div class=" lg:h-[260px]  xs:flex  xs:flex-col-reverse  lg:flex-row xs:bg-blue-950 lg:bg-white  xs:p-4 xs:rounded-2xl  ">
+         <div className=" lg:w-1/2 h-[260px]  xs:w-full xs:bg-black lg:bg-white xs:text-blue-300 lg:text-black xs:rounded-2xl ">
          <h1 className="text-2xl font-bold font-AfacadBold text-center mb-1">{car.name}</h1>
         <div className="flex gap-4 justify-center items-center font-AfacadRegular text-xl  ">
         <h1 className="text-2xl font-bold font-AfacadBold underline  mb-4  bg-amber-300 h-1 w-22 text-center"></h1>
@@ -55,11 +55,11 @@ const SelectCar = () => {
          <div className="flex gap-4 justify-center items-center font-AfacadRegular text-xl mb-2 "><button  className="text-gray-500 py-1 px-5 border-1 border-gray-500">Book Now</button></div>
          </div>
 
-         <div className="bg-[#ADDEE5] w-1/2 h-[260px] flex justify-center items-center relative">
+         <div className="bg-[#ADDEE5] lg:w-1/2 xs:w-full h-[260px] rounded-2xl flex justify-center items-center relative">
            <img
              src={car.image}
              alt=""
-             className="absolute -right-6 w-[800px]"
+             className="absolute lg:-right-6 lg:w-[800px] xs:w-[240px]"
            />
            <h2 className="underline absolute bottom-6">Details</h2>
          </div>
@@ -68,39 +68,7 @@ const SelectCar = () => {
        )}
       </div>
       
-      {/* <div class="flex flex-col lg:flex-row gap-5 flex-wrap ">
-       {carData.map((car) => (
-
-         <div class="  flex-col-reverse   ">
-
-         <div className=" w-full h-[260px]">
-         <h1 className="text-2xl font-bold font-AfacadBold text-center mb-1">{car.name}</h1>
-        <div className="flex gap-4 justify-center items-center font-AfacadRegular text-xl  ">
-        <h1 className="text-2xl font-bold font-AfacadBold underline  mb-4  bg-amber-300 h-1 w-22 text-center"></h1>
-
-        </div >
-         <div className="flex gap-4 justify-center items-center font-AfacadRegular text-xl mb-3 "><IoMdPeople className="text-4xl" /><h1 className="text-gray-500">Persons-{car.persons}</h1></div>
-         <div className="flex gap-4 justify-center items-center font-AfacadRegular text-xl mb-3 "><BsSuitcaseLgFill className="text-3xl" /><h1  className="text-gray-500">Luggage-{car.luggage}</h1></div>
-         <div className="flex gap-4 justify-center items-center font-AfacadRegular text-xl mb-4 "><IoLogoNoSmoking className="text-4xl" /><h1  className="text-gray-500 -mr-3">No Smoking</h1></div>
-         <div className="flex gap-4 justify-center items-center font-AfacadRegular text-xl mb-2 "><button  className="text-gray-500 py-1 px-5 border-1 border-gray-500">Book Now</button></div>
-         </div>
-
-         <div className="bg-[#ADDEE5] w-full  h-[200px] rounded-2xl flex justify-center items-center mb-4 relative ">
-           <img
-             src={car.image}
-             alt=""
-             className="absolute  w-[240px]"
-           />
-           <h2 className="underline absolute bottom-6">Details</h2>
-         </div>
-
-       </div>
-
-
-        )
-       )}
-      </div>
-       */}
+    
       
       
       
